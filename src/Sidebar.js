d83bc4e4415d55fe1a8ae1f582df7fd8b9021811
Octowl <alsaff1987@gmail.com>
2019-09-24 10:18:41 +0300

    move redux stuff into folder

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { addAuthor } from "./actions";
+import { addAuthor } from "./stores/actions";
 
 const Sidebar = props => {
   return (
